970e1e3322b2d6dbd636da0d796944292503a7d9
Juniorndlovu <wedsonndlovu5@gmail.com>
2024-08-10 12:13:55 +0200

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,11 @@
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# download and install Node.js (you may need to restart the terminal)
+nvm install 20
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.16.0`
+
+# verifies the right npm version is in the environment
+npm -v # should print `10.8.1`
